171efd5c8907202ece780ae689ac69afc5ef1282
Dani <danmares4312@gmail.com>
2025-11-05 13:35:37 -0500

    complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  // handle variable num of arguments
+  let array;
+  let itemsToRemove = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+    if (i == 0) array = arguments[0];
+    else itemsToRemove.push(arguments[i]);
+  }
+
+  itemsToRemove.forEach(
+    (item) => (array = array.filter((value) => value !== item))
+  );
+  return array;
 };
 
 // Do not edit below this line
