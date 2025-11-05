ce899296c3047633e81850afd71794d2acc3bfd8
Dani <danmares4312@gmail.com>
2025-11-05 13:08:06 -0500

    complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (str) => {
+  let reverseStr = "";
+  for (let i = str.length; i >= 0; i--) {
+    reverseStr += str.charAt(i);
+  }
+  return reverseStr;
 };
 
 // Do not edit below this line
