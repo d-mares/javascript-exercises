09aaab0e5613c4ef39f9bc38abdce1063f1fb2a5
Dani <danmares4312@gmail.com>
2025-11-05 14:25:05 -0500

    complete 09_sumAll
    This is a little harder to read but I wanted to
    commit to my inital solution, testing my ability
    to use some of these unfamiliar array methods.
    
    I am iterating through the two arguments ensuring
    they are integers greater than 0, if they are not
    the some method is able to break from the loop and
    return a value, unlike the forEach method.
    
    in the case where the conditional is true, it sets
    the isInvalid flag as true and then returns error.
    
    A cleaner solution would break this down and also
    set min  and max rather than rewriting the same
    code but in reverse

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
+  total = 0;
 
+  let isInvalid = [num1, num2].some((num) => num < 0 || !Number.isInteger(num));
+
+  if (isInvalid) return "ERROR";
+  if (num2 > num1) {
+    for (let i = num1; i <= num2; i++) {
+      total += i;
+    }
+    return total;
+  } else {
+    for (let i = num2; i <= num1; i++) {
+      total += i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
