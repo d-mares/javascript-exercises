c05ef9239488d966b3343b1bd9077049ebcbd395
Dani <danmares4312@gmail.com>
2025-11-13 11:06:51 -0500

    complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length == 0) return 0;
+  return arr.reduce((total, num) => num + total);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  if (arr.length == 0) return 0;
+  return arr.reduce((total, num) => num * total);
 };
 
-const power = function() {
-	
+const power = function (b, e) {
+  return b ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n == 1 || n == 0) return 1;
+  if (n > 1) return n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +32,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
