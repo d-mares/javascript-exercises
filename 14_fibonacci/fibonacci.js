53a2fa03d64f171517b32b8b7826889ec3876e1b
Dani <danmares4312@gmail.com>
2025-11-13 17:03:43 -0500

    complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = (n) => {
+  if (n == 0) return 0;
+  if (n < 0) return "OOPS";
+  let count = 2;
+  let fib = [1, 1];
+  while (count < n) {
+    console.log(fib.at(-2), fib.at(-1));
+    fib.push(fib.at(-2) + fib.at(-1));
+    count++;
+  }
+  console.log(fib.at(-2), fib.at(-1));
+  return fib.at(n - 1);
 };
 
 // Do not edit below this line
