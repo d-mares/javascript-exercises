713919b3a5bcb95c558fa2676460d53e811c38dd
Dani <danmares4312@gmail.com>
2025-11-13 16:18:32 -0500

    complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let cleaned_str = "",
+    palindrome = "";
+  for (let i = 0; i < str.length; i++) {
+    if ([",", " ", "?", ".", "!"].includes(str[i])) continue;
+    cleaned_str = `${cleaned_str + str[i].toLowerCase()}`;
+    console.log(cleaned_str);
+  }
+  for (let k = cleaned_str.length - 1; k >= 0; k--) {
+    palindrome = `${palindrome + cleaned_str[k].toLowerCase()}`;
+    console.log(palindrome);
+  }
+  return palindrome == cleaned_str;
 };
 
 // Do not edit below this line
